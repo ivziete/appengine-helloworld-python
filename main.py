edd5db3412a2f0a77dfa8157247fbb38a116d813
Ivan Condori <ivziete@gmail.com>
2014-07-12 12:00:39 -0500

    cambio mensaje1

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hola Mundo Github!')
+        self.response.write('Hola Mundo Github1!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
