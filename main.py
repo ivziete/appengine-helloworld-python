6b53a16014819e632f93f0c48ee98369036469e6
Ivan Condori <ivziete@gmail.com>
2014-07-12 12:39:25 -0500

    otro mensaje

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hola Papa!')
+        self.response.write('<h1>Hola Universo!!</h1>')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
